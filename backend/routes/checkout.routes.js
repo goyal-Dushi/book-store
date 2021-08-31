2ba2b73bb249fafe6927d74717fdcf881d25f947
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-08-31 23:35:09 +0530

    modified route to check if seller and buyer id is same

--- backend/routes/checkout.routes.js
+++ backend/routes/checkout.routes.js
@@ -12,19 +12,21 @@ router.route("/checkout/:id").patch((req, res) => {
         res.status(403).json({ msg: "Unable to update book's details" });
       }
     });
-    User.updateOne(
-      { _id: book.sellerID },
-      { $push: { soldList: book } },
-      (err) => {
-        if (err) {
-          res
-            .status(403)
-            .json({ msg: "Unable to update Sold list details!", error: err });
-        } else {
-          res.status(200).json({ msg: "Successfully updated Sold List" });
+    if (book?.sellerID === custID) {
+      User.updateOne(
+        { _id: book.sellerID },
+        { $push: { soldList: book, boughtList: book } },
+        (err) => {
+          if (err) {
+            res
+              .status(403)
+              .json({ msg: "Unable to update Sold list details!", error: err });
+          } else {
+            res.status(200).json({ msg: "Successfully updated List" });
+          }
         }
-      }
-    );
+      );
+    }
   });
   User.updateOne({ _id: custID }, { $push: { boughtList: books } }, (err) => {
     if (err) {
