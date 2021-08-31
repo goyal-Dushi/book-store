4be8ac69ef6b19865cc909c6144962483ccf4345
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-08-31 23:32:59 +0530

    added delete route and modified books by seller route

--- backend/routes/books.routes.js
+++ backend/routes/books.routes.js
@@ -7,7 +7,21 @@ router.route("/").get((req, res) => {
     if (err) {
       res.status(400).json({ msg: "Not able to find books", error: err });
     } else {
-      res.status(200).json({ books: data });
+      res.status(200).json({ data });
+    }
+  });
+});
+
+router.route("/:id").get((req, res) => {
+  const id = req.params.id;
+
+  Book.find({ sellerID: id }, (err, data) => {
+    if (err) {
+      res
+        .status(400)
+        .json({ msg: "Not able to fetch seller's books", error: err });
+    } else {
+      res.status(200).json({ data });
     }
   });
 });
@@ -31,6 +45,18 @@ router.route("/edit/:id").patch((req, res) => {
       res.status(400).json({ msg: "Book update error", error: err });
     } else {
       res.status(200).json({ msg: "Book details updated Successfully!" });
+    }
+  });
+});
+
+router.route("/delete/:id").delete((req, res) => {
+  const id = req.params.id;
+
+  Book.findByIdAndDelete(id, (err, data) => {
+    if (err) {
+      res.status(402).json({ msg: "Book delete Error", error: err });
+    } else {
+      res.status(200).json({ msg: `Successfully removed ${data.name}` });
     }
   });
 });
