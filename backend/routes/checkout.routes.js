6584fbcd95006488ba4b35e439f420360aaa89dd
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-09-01 14:25:17 +0530

    removed if condition

--- backend/routes/checkout.routes.js
+++ backend/routes/checkout.routes.js
@@ -3,30 +3,28 @@ const router = express.Router();
 const Book = require("../models/books.model");
 const User = require("../models/users.model");
 
-router.route("/checkout/:id").patch((req, res) => {
+router.route("/checkout/:id").put((req, res) => {
   const custID = req.params.id;
   const books = req.body;
-  books.map((book) => {
+  books.forEach((book) => {
     Book.updateOne({ _id: book._id }, { $set: { ...book } }, (err) => {
       if (err) {
         res.status(403).json({ msg: "Unable to update book's details" });
       }
     });
-    if (book?.sellerID === custID) {
-      User.updateOne(
-        { _id: book.sellerID },
-        { $push: { soldList: book, boughtList: book } },
-        (err) => {
-          if (err) {
-            res
-              .status(403)
-              .json({ msg: "Unable to update Sold list details!", error: err });
-          } else {
-            res.status(200).json({ msg: "Successfully updated List" });
-          }
+    User.updateOne(
+      { _id: book.sellerID },
+      { $push: { soldList: book } },
+      (err) => {
+        if (err) {
+          res
+            .status(403)
+            .json({ msg: "Unable to update Sold list details!", error: err });
+        } else {
+          res.status(200).json({ msg: "Successfully updated List" });
         }
-      );
-    }
+      }
+    );
   });
   User.updateOne({ _id: custID }, { $push: { boughtList: books } }, (err) => {
     if (err) {
