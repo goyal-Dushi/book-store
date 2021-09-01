7c809fd6157cf64946374742e197f45c9fb983af
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-09-01 14:27:56 +0530

    updated routing for getting profile data and books data

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -12,6 +12,19 @@ function ProfilePage() {
   const [booksBySeller, setBooksBySeller] = useState([]);
   const [modalOpen, setModalOpen] = useState({ status: false, type: "" });
 
+  console.log("userProfile: ", userProfile);
+
+  const getListedBooks = async () => {
+    const response = await axios
+      .get("http://localhost:5000/books/" + id)
+      .then((res) => res.data.data)
+      .catch((err) => {
+        console.log("Error getting seller books: ", err);
+      });
+    console.log("GET LISTED BOOKS: ", response);
+    setBooksBySeller(response);
+  };
+
   useEffect(() => {
     const getUser = async () => {
       const userData = await axios
@@ -24,38 +37,30 @@ function ProfilePage() {
       setUserProfile(userData.data);
     };
     getUser();
+    // getListedBooks();
   }, [id]);
 
-  console.log("userProfile: ", userProfile);
-
   useEffect(() => {
-    if (modalOpen.type === "add" && !modalOpen.status) {
-      const getBooks = async () => {
-        const response = await axios
-          .get("http://localhost:5000/books/" + id)
-          .then((res) => res.data)
-          .catch((err) => {
-            console.log("Error getting books: ", err);
-          });
-        setBooksBySeller(response.data);
-      };
-      getBooks();
+    if (!modalOpen.status) {
+      getListedBooks();
     }
-  }, [modalOpen]);
+  }, [modalOpen.status]);
 
-  const getListedBooks = async () => {
-    const response = await axios
-      .get("http://localhost:5000/books/" + userProfile?._id)
-      .then((res) => res.data)
-      .catch((err) => {
-        console.log("Error getting seller books: ", err);
-      });
-    return response;
+  const containerStyle = {
+    display: "flex",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  };
+
+  const addBook = () => {
+    setModalOpen({ type: "add", status: true });
+    getListedBooks();
   };
 
   const editBook = (book) => {
     setBookToEdit(book);
     setModalOpen({ status: true, type: "edit" });
+    getListedBooks();
   };
 
   const deleteBook = async (book) => {
@@ -68,9 +73,6 @@ function ProfilePage() {
     console.log(response);
     getListedBooks();
   };
-
-  console.log(modalOpen);
-
   return (
     <>
       <h4 className={"display-4 mb-3"}> {"Welcome " + userProfile?.name} </h4>
@@ -81,16 +83,14 @@ function ProfilePage() {
         address={userProfile?.address}
       />
       {userProfile?.isSeller ? (
-        <Button
-          onClick={() => setModalOpen({ type: "add", status: true })}
-          variant={"outline-success"}>
+        <Button onClick={() => addBook()} variant={"outline-success"}>
           {"Add Books"}
         </Button>
       ) : null}
       {modalOpen.status ? (
         <Modal className={"p-3"} show={modalOpen.status}>
           <Modal.Header>
-            {modalOpen.type === "edit" ? "Edit Book" : "Add Book"}{" "}
+            {modalOpen.type === "edit" ? "Edit Book" : "Add Book"}
           </Modal.Header>
           <ModalBody>
             {modalOpen.type === "edit" ? (
@@ -112,33 +112,10 @@ function ProfilePage() {
         </Modal>
       ) : null}
       <Container fluid={"md"} className={"mt-3"}>
-        {booksBySeller?.length ? (
-          <>
-            <h3 className={"display-5 mt-2 mb-3"}> {"Books Listed By You"} </h3>
-            <Container
-              fluid={"md"}
-              style={{ display: "flex", justifyContent: "space-evenly" }}>
-              {booksBySeller?.forEach((book, i) => (
-                <Cards
-                  key={i}
-                  type={"bookSold"}
-                  name={book?.name}
-                  sellerName={book?.sellerName}
-                  sellerAddress={book?.sellerAddress}
-                  address={"Not yet Sold"}
-                  price={book?.price}
-                  soldOn={"Not Yet sold"}
-                />
-              ))}
-            </Container>
-          </>
-        ) : null}
         {userProfile?.soldList?.length ? (
           <>
             <h3 className={"display-5 mb-2"}> {"Books Sold"} </h3>
-            <Container
-              fluid={"md"}
-              style={{ display: "flex", justifyContent: "space-evenly" }}>
+            <Container fluid={"md"} style={{ ...containerStyle }}>
               {userProfile?.soldList?.map((book, i) => (
                 <Cards
                   key={i}
@@ -156,54 +133,65 @@ function ProfilePage() {
         ) : (
           <h3 className={"display-5 mb-2"}> {"No Books Sold yet!"} </h3>
         )}
+      </Container>
+      <Container>
         {userProfile?.boughtList?.length ? (
           <>
             <h3 className={"display-5 mb-2 mt-3"}> {"Books Bought"} </h3>
-            {userProfile?.boughtList?.map((book, i) => (
-              <Cards
-                key={i}
-                type={"bookBought"}
-                name={book?.name}
-                sellerName={book?.sellerName}
-                sellerAddress={book?.sellerAddress}
-                address={userProfile?.address}
-                price={book?.price}
-                soldOn={book?.soldOn}
-              />
-            ))}
+            <Container fluid={"md"} style={{ ...containerStyle }}>
+              {userProfile?.boughtList?.map((book, i) => (
+                <Cards
+                  key={i}
+                  type={"bookBought"}
+                  name={book?.name}
+                  sellerName={book?.sellerName}
+                  sellerAddress={book?.sellerAddress}
+                  address={userProfile?.address}
+                  price={book?.price}
+                  soldOn={book?.soldOn}
+                />
+              ))}
+            </Container>
           </>
         ) : (
           <h3 className={"display-5 mb-2 mt-3"}> {"No Books Bought yet!"} </h3>
         )}
       </Container>
       <Container className={"mt-2"} fluid={"md"}>
-        {getListedBooks().length
-          ? getListedBooks()?.forEach((book, i) => (
-              <Card key={i} style={{ width: "fit-content" }}>
-                <Card.Body>
-                  <Cards
-                    type={"book"}
-                    name={book?.name}
-                    sellerName={book?.sellerName}
-                    sellerAddress={book?.sellerAddress}
-                    price={book?.price}
-                    addToCart={false}
-                  />
-                </Card.Body>
-                <Card.Footer>
-                  <Button onClick={() => editBook(book)} variant={"warning"}>
-                    {"Edit"}
-                  </Button>
-                  <Button
-                    onClick={() => deleteBook(book)}
-                    className={"ml-2"}
-                    variant={"outline-danger"}>
-                    {"Delete"}
-                  </Button>
-                </Card.Footer>
-              </Card>
-            ))
-          : null}
+        {booksBySeller?.length ? (
+          <>
+            <h3 className={"display-5 mb-2 mt-3"}> {"Books Listed By You"} </h3>
+            <Container fluid={"md"} style={{ ...containerStyle }}>
+              {booksBySeller?.map((book, i) => (
+                <Card key={i} style={{ width: "fit-content" }}>
+                  <Card.Body>
+                    <Cards
+                      type={"bookListed"}
+                      name={book?.name}
+                      sellerName={book?.sellerName}
+                      sellerAddress={book?.sellerAddress}
+                      price={book?.price}
+                      stock={book?.stock}
+                    />
+                  </Card.Body>
+                  <Card.Footer>
+                    <Button onClick={() => editBook(book)} variant={"warning"}>
+                      {"Edit"}
+                    </Button>
+                    <Button
+                      onClick={() => deleteBook(book)}
+                      className={"ml-2"}
+                      variant={"outline-danger"}>
+                      {"Delete"}
+                    </Button>
+                  </Card.Footer>
+                </Card>
+              ))}
+            </Container>
+          </>
+        ) : (
+          <h3 className={"display-5 mb-2 mt-3"}>{"No Books Listed By You!"}</h3>
+        )}
       </Container>
       <Link to={"/booklist/" + id} style={{ textDecoration: "none" }}>
         <Button variant={"outline-primary"}>{"Checkout Products"}</Button>
