2e3506a1cebf61fb751c8bdb39e8e123aca65b4e
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-09-08 22:45:04 +0530

    removed comments and other non useful lib imports

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
 const PORT = 5000;
 const UserRouter = require("./routes/users.routes");
 const BooksRouter = require("./routes/books.routes");
@@ -9,17 +12,40 @@ const CheckoutRouter = require("./routes/checkout.routes");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
-mongoose.connect("mongodb://localhost:27017/bookStore", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 
-db.once("open", () => {
-  console.log("Database connection established!");
-});
+app.use(
+  session({
+    secret: "somesecrestofBookstore@123%78237dfnisn",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+const mongoURI = "mongodb://localhost:27017/bookStore";
+
+const connectDB = async () => {
+  await mongoose.connect(
+    mongoURI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    () => {
+      console.log("Database connnection established!");
+    }
+  );
+};
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Home page, Check console!");
