b8e8f93d77cc56b082bf751156b6e6ac398d1480
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-09-08 22:52:02 +0530

    used usercontext for uid initialization

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import UserContext from "./components/contexts/userContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <UserContext>
+    <App />
+  </UserContext>,
+  document.getElementById("root")
+);
