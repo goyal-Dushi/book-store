77b898a6e09f3bd70548366627a43f9827a3df24
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-09-08 22:53:19 +0530

    added coditional rendering of routes and included contexts

--- src/App.js
+++ src/App.js
@@ -6,21 +6,40 @@ import UserProfile from "./pages/UserProfile";
 import BookList from "./pages/BookList";
 import Checkout from "./pages/Checkout";
 import Home from "./pages/Home";
+import Alert from "./components/alert";
+import AlertContextBox from "./components/contexts/alertContext";
+import AppNavbar from "./components/navbar";
+import { UserDetailsContext } from "./components/contexts/userContext";
+import { useContext } from "react";
 
 function App() {
+  const { uid } = useContext(UserDetailsContext);
   return (
-    <Container fluid={"md"}>
-      <Router>
-        <Switch>
-          <Route path={"/"} exact component={Home} />
-          <Route path={"/login"} exact component={Login} />
-          <Route path={"/register"} exact component={RegisterPage} />
-          <Route path={"/profile/:id"} component={UserProfile} />
-          <Route path={"/booklist/:id"} component={BookList} />
-          <Route path={"/cartCheckout/:id"} component={Checkout} />
-        </Switch>
-      </Router>
-    </Container>
+    <>
+      <AlertContextBox>
+        <Router>
+          <AppNavbar />
+          <Container fluid={"md"}>
+            <Alert />
+            <Switch>
+              <Route path={"/"} exact component={Home} />
+              {uid ? (
+                <>
+                  <Route path={"/profile"} component={UserProfile} />
+                  <Route path={"/booklist"} component={BookList} />
+                  <Route path={"/cartCheckout"} component={Checkout} />
+                </>
+              ) : (
+                <>
+                  <Route path={"/login"} exact component={Login} />
+                  <Route path={"/register"} exact component={RegisterPage} />
+                </>
+              )}
+            </Switch>
+          </Container>
+        </Router>
+      </AlertContextBox>
+    </>
   );
 }
 
