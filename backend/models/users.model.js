28c9bacaf81a4ee74476f6dbd2ece95f80b1b500
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-09-09 22:31:51 +0530

    removed passport-local-mongoose dependency

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Book = require("./books.model");
-const passportLocalMongoose = require("passport-local-mongoose");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -33,7 +32,6 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-UserSchema.plugin(passportLocalMongoose);
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
