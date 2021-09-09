e53c41cbc93c4eac52c3234276ad4c927ceb2f12
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-09-09 22:34:19 +0530

    updated usercontext values

--- src/components/contexts/userContext.js
+++ src/components/contexts/userContext.js
@@ -1,31 +1,11 @@
-import axios from "axios";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 
 export const UserDetailsContext = createContext();
 
 function UserContext(props) {
   const [userData, setUserData] = useState({});
-  const [uid, setUID] = useState("");
-  console.log("uid of user: ", uid);
-  useEffect(() => {
-    const getUser = async () => {
-      const info = await axios
-        .get("http://localhost:5000/users/profile/" + uid, {
-          withCredentials: true,
-        })
-        .then((res) => res.data)
-        .catch((err) => {
-          console.log("Error fetching user: ", err);
-        });
-      console.log("Usercontext info: ", info);
-      setUserData(info?.data);
-    };
-    if (uid) {
-      getUser();
-    }
-  }, [uid]);
   return (
-    <UserDetailsContext.Provider value={{ userData, setUID, uid }}>
+    <UserDetailsContext.Provider value={{ userData, setUserData }}>
       {props.children}
     </UserDetailsContext.Provider>
   );
