bc45f8fb00b1f7c26fceec0608ce9cfe4f837d76
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-09-09 22:29:01 +0530

    removed log statements and serialized complete user info

--- backend/config/passportConfig.js
+++ backend/config/passportConfig.js
@@ -5,17 +5,16 @@ const User = require("../models/users.model");
 module.exports = function (passport) {
   passport.use(
     new passportLocalStrategy((username, password, done) => {
-      console.log("In passport config.js");
       User.findOne({ name: username }, (err, data) => {
         if (err) {
           throw err;
         }
         if (!data) {
-          console.log("Passport auth local !data: ", data);
           return done(null, false);
         }
-        console.log("Passport auth local data: ", data);
         bcrypt.compare(password, data.password, (err, result) => {
+          console.log("Error in bcrypt compare: ", err);
+          console.log("result in bcrypt compare: ", result);
           if (err) {
             throw err;
           } else if (result) {
@@ -31,7 +30,7 @@ module.exports = function (passport) {
 
   passport.serializeUser((user, cb) => {
     console.log("Serialize user: ", user);
-    cb(null, user._id);
+    cb(null, user);
   });
   passport.deserializeUser((id, cb) => {
     console.log("Deseialize user: ", id);
