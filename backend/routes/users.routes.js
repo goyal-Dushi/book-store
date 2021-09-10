0c15184a9f80deca296c2372a2056327e484f469
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-09-10 21:38:00 +0530

    added controller for GET req of login and removed GET req for register

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -6,18 +6,11 @@ const userController = require("../controllers/user.controller");
 
 router.route("/").get(userController.getAll_users);
 
-router
-  .route("/register")
-  .get((req, res) => {
-    console.log("User Registration page");
-  })
-  .post(userController.user_register);
+router.route("/register").post(userController.user_register);
 
 router
   .route("/login")
-  .get((req, res) => {
-    console.log("User Login page");
-  })
+  .get(userController.check_user_login)
   .post(passport.authenticate("local"), userController.user_login);
 
 router.route("/logout").get(userController.user_logout);
