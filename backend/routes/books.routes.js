e92a24df1401fd76309c0c0880c17a58e668b9c1
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-09-10 21:44:01 +0530

    removed logic for getting books by seller to respective controller: get_books_by_seller

--- backend/routes/books.routes.js
+++ backend/routes/books.routes.js
@@ -1,25 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const booksController = require("../controllers/books.controller");
-const Book = require("../models/books.model");
 
 router.route("/").get(booksController.getAll_books);
 router.route("/add").post(booksController.add_book);
 router.route("/edit/:id").patch(booksController.edit_book);
 router.route("/delete/:id").delete(booksController.delete_book);
-
-router.route("/:id").get((req, res) => {
-  const id = req.params.id;
-
-  Book.find({ sellerID: id }, (err, data) => {
-    if (err) {
-      res
-        .status(400)
-        .json({ msg: "Not able to fetch seller's books", error: err });
-    } else {
-      res.status(200).json({ data });
-    }
-  });
-});
+router.route("/:id").get(booksController.get_books_by_seller);
 
 module.exports = router;
