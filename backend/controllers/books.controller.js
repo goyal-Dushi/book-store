d88b5df7da75371d8bb39f5cc5219049527a2ec0
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-09-10 21:38:45 +0530

    added get_books_by_seller, updated status codes

--- backend/controllers/books.controller.js
+++ backend/controllers/books.controller.js
@@ -6,7 +6,7 @@ const add_book = (req, res) => {
     if (err) {
       res.status(400).json({ msg: "Not able to add book!", error: err });
     } else {
-      res.status(200).json({ msg: "Book added successfully" });
+      res.status(201).json({ msg: "Book added successfully" });
     }
   });
 };
@@ -18,7 +18,7 @@ const edit_book = (req, res) => {
     if (err) {
       res.status(400).json({ msg: "Book update error", error: err });
     } else {
-      res.status(200).json({ msg: "Book details updated Successfully!" });
+      res.status(201).json({ msg: "Book details updated Successfully!" });
     }
   });
 };
@@ -45,9 +45,28 @@ const getAll_books = (req, res) => {
   });
 };
 
+const get_books_by_seller = (req, res) => {
+  const id = req.params.id;
+
+  Book.find({ sellerID: id }, (err, data) => {
+    if (err) {
+      res
+        .status(400)
+        .json({ msg: "Not able to fetch seller's books", error: err });
+    } else if (!data) {
+      res.status(200).json({
+        status: false,
+      });
+    } else {
+      res.status(200).json({ status: true, data: data });
+    }
+  });
+};
+
 module.exports = {
   add_book,
   edit_book,
   delete_book,
   getAll_books,
+  get_books_by_seller,
 };
