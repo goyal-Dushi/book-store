848eee45c078b8a8aa7719efa21abfcd777196da
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-09-16 20:31:22 +0530

    conditional rendering of books based on Product list, removed log statements, removed userProfile state dependency on route

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,115 +1,136 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { Card, Button, Container } from "react-bootstrap";
-import { Link, useHistory, useLocation, useParams } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import Cards from "../components/cards";
+import { AlertContext } from "../components/contexts/alertContext";
+import { UserDetailsContext } from "../components/contexts/userContext";
+
+const flexEvenlyCenter = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-evenly",
+};
+
+const footerStyle = {
+  width: "100%",
+  marginTop: "50px",
+};
 
 function Checkout() {
-  const [bookTotal, setTotal] = useState(0);
   const location = useLocation();
-  const [checkedBooks, setCheckedBooks] = useState([]);
-  const { id } = useParams();
+  const [booksDetails, setBooksDetails] = useState({ total: 0, list: [] });
+  const { setAlertState } = useContext(AlertContext);
+  const { userData, setUserData } = useContext(UserDetailsContext);
   const history = useHistory();
-  const handleRemove = (index) => {
-    let items = [...checkedBooks];
-    console.log("Before remove: ", items);
-    items.splice(index, 1);
-    console.log("After Remove: ", items);
-    setCheckedBooks(items);
-  };
 
-  const containerStyle = {
-    display: "flex",
-    justifyContent: "space-evenly",
-    alignItems: "center",
+  // removing books from list
+  const handleRemove = (index) => {
+    let items = [...booksDetails.list];
+    items.splice(index, 1);
+    const total = items.reduce((acc, book) => {
+      return (acc += book?.price);
+    }, 0);
+    setBooksDetails({ total: total, list: [...items] });
   };
 
   useEffect(() => {
-    if (JSON.parse(location?.state?.bookList).length) {
-      setCheckedBooks(JSON.parse(location?.state?.bookList));
+    const getUserProfile = async () => {
+      const userInfo = await axios
+        .get("http://localhost:5000/users/login", { withCredentials: true })
+        .then((res) => res.data)
+        .catch((err) => {
+          console.log("Error: ", err);
+        });
+      setUserData(userInfo?.data);
+    };
+    getUserProfile();
+  }, []);
+
+  useEffect(() => {
+    const books = JSON.parse(location?.state?.bookList);
+    if (books?.length) {
+      const total = books.reduce((acc, book) => {
+        return (acc += book?.price);
+      }, 0);
+      setBooksDetails({
+        total: total,
+        list: [...books],
+      });
     }
   }, [location?.state?.bookList]);
 
-  useEffect(() => {
-    const total = checkedBooks.reduce((acc, book) => {
-      return (acc += book?.price);
-    }, 0);
-    setTotal(total);
-  }, [checkedBooks]);
-
   const handleCheckout = async () => {
-    if (checkedBooks?.length) {
-      const newList = checkedBooks?.map((book) => {
+    if (booksDetails.list?.length) {
+      const newList = booksDetails.list?.map((book) => {
         let newStock = book?.stock;
         newStock -= 1;
-        return { ...book, stock: newStock, soldOn: new Date().toDateString() };
+        return {
+          ...book,
+          stock: newStock,
+          soldOn: new Date().toDateString(),
+          boughtBy: userData?.name,
+          address: userData?.address,
+        };
       });
       const response = await axios
-        .put("http://localhost:5000/checkout/" + id, newList)
+        .put("http://localhost:5000/checkout/" + userData?._id, newList, {
+          withCredentials: true,
+        })
         .then((res) => res.data)
         .catch((err) => {
           console.log("Error Book Checkout: ", err);
         });
-      console.log(response);
-      setTotal(0);
-      setCheckedBooks([]);
-      history.push("/profile/" + id);
+      setBooksDetails({ total: 0, list: "" });
+      setAlertState({ type: "success", show: true, msg: response?.msg });
+      history.replace("/profile");
     }
     return;
   };
 
   return (
     <>
-      <h5 className={"display-5 mb-3"}> {"Buy & Checkout"} </h5>
-      <Container
-        fluid={"md"}
-        style={{
-          ...containerStyle,
-          flexDirection: "column",
-          height: "100%",
-        }}>
-        {checkedBooks?.length ? (
-          checkedBooks?.map((book, i) => (
-            <Card key={i}>
-              <Card.Body>
-                <Cards type={"book"} bookData={book} />
-              </Card.Body>
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-evenly",
-                }}>
-                <Button
-                  onClick={() => handleRemove(i)}
-                  variant={"outline-danger"}>
-                  {"Remove"}
-                </Button>
-              </div>
-            </Card>
-          ))
-        ) : (
-          <h3 className={"display-4 mt-2 mb-2"}> {"No books bought Yet!"} </h3>
-        )}
-        <footer
-          style={{
-            width: "100%",
-            bottom: "0",
-            marginTop: "50px",
-            marginBottom: "0px",
-          }}>
-          <Container style={{ ...containerStyle }} fluid={"md"}>
-            <div> {`Total: ${bookTotal}`} </div>
-            <Link style={{ textDecoration: "none" }}>
-              <Button
-                onClick={() => handleCheckout()}
-                variant={"outline-success"}>
-                {"Buy & Pay"}
-              </Button>
-            </Link>
+      {userData?._id ? (
+        <>
+          <h5 className={"display-5 mb-3"}> {"Buy & Checkout"} </h5>
+          <Container
+            fluid={"md"}
+            style={{ ...flexEvenlyCenter, flexWrap: "wrap" }}>
+            {booksDetails?.list?.length ? (
+              booksDetails?.list?.map((book, i) => (
+                <Card key={i}>
+                  <Card.Body>
+                    <Cards type={"book"} bookData={book} />
+                  </Card.Body>
+                  <div style={{ ...flexEvenlyCenter }}>
+                    <Button
+                      onClick={() => handleRemove(i)}
+                      variant={"outline-danger"}>
+                      {"Remove"}
+                    </Button>
+                  </div>
+                </Card>
+              ))
+            ) : (
+              <h3 className={"display-4 mt-2 mb-2"}>
+                {"No books bought Yet!"}
+              </h3>
+            )}
+            <footer style={{ ...footerStyle }}>
+              {booksDetails?.list?.length ? (
+                <Container style={{ ...flexEvenlyCenter }} fluid={"md"}>
+                  <div> {`Total: ${booksDetails.total}`} </div>
+                  <Button
+                    onClick={() => handleCheckout()}
+                    variant={"outline-success"}>
+                    {"Buy & Pay"}
+                  </Button>
+                </Container>
+              ) : null}
+            </footer>
           </Container>
-        </footer>
-      </Container>
+        </>
+      ) : null}
     </>
   );
 }
