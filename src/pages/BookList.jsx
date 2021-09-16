9ada118226b46d9f88e721491faf43ca2be01103
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-09-16 20:36:32 +0530

    used context api to fetch books, conditional rendering of component applied, vaidating user session on refresh applied

--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -1,13 +1,15 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { Container, FormControl, InputGroup, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Cards from "../components/cards";
+import { UserDetailsContext } from "../components/contexts/userContext";
 
 function BookList() {
   const [books, setBooks] = useState([]);
   const [cartItem, setCartItem] = useState([]);
   const [search, setSearch] = useState("");
+  const { userData, setUserData } = useContext(UserDetailsContext);
 
   useEffect(() => {
     const getBooks = async () => {
@@ -21,6 +23,16 @@ function BookList() {
     };
     getBooks();
     document.title = "All Books!";
+    const getUserProfile = async () => {
+      const userInfo = await axios
+        .get("http://localhost:5000/users/login", { withCredentials: true })
+        .then((res) => res.data)
+        .catch((err) => {
+          console.log("Error: ", err);
+        });
+      setUserData(userInfo?.data);
+    };
+    getUserProfile();
   }, []);
 
   const searchBook = (books) => {
@@ -43,54 +55,58 @@ function BookList() {
 
   return (
     <>
-      <Container
-        className={"d-flex justify-content-between"}
-        style={{ flexWrap: "wrap" }}
-        fluid={"md"}>
-        <h4 className={"display-5"}> {"Search for Your Fav books"} </h4>
-        <Link
-          to={{
-            pathname: `/cartCheckout`,
-            state: {
-              bookList: JSON.stringify(cartItem),
-            },
-          }}
-          style={{ textDecoration: "none" }}>
-          <Button variant={"dark"}>
-            {`Proceed to checkout : ${cartItem?.length}`}
-          </Button>
-        </Link>
-      </Container>
-      <Container fluid={"md"} className={"mt-3"}>
-        <InputGroup className={"mt-2 mb-3"}>
-          <FormControl
-            placeholder={"Search books here!"}
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-        </InputGroup>
+      {userData?._id ? (
+        <>
+          <Container
+            className={"d-flex justify-content-between"}
+            style={{ flexWrap: "wrap" }}
+            fluid={"md"}>
+            <h4 className={"display-5"}> {"Search for Your Fav books"} </h4>
+            <Link
+              to={{
+                pathname: `/cartCheckout`,
+                state: {
+                  bookList: JSON.stringify(cartItem),
+                },
+              }}
+              style={{ textDecoration: "none" }}>
+              <Button variant={"dark"}>
+                {`Proceed to checkout : ${cartItem?.length}`}
+              </Button>
+            </Link>
+          </Container>
+          <Container fluid={"md"} className={"mt-3"}>
+            <InputGroup className={"mt-2 mb-3"}>
+              <FormControl
+                placeholder={"Search books here!"}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+            </InputGroup>
 
-        <Container
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-evenly",
-            flexWrap: "wrap",
-          }}>
-          {search.length
-            ? searchBook(books)
-            : books?.map((book, i) => (
-                <Cards
-                  key={i}
-                  type={"book"}
-                  bookData={book}
-                  addToCart={true}
-                  cartItem={cartItem}
-                  setCartItem={setCartItem}
-                />
-              ))}
-        </Container>
-      </Container>
+            <Container
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-evenly",
+                flexWrap: "wrap",
+              }}>
+              {search.length
+                ? searchBook(books)
+                : books?.map((book, i) => (
+                    <Cards
+                      key={i}
+                      type={"book"}
+                      bookData={book}
+                      addToCart={true}
+                      cartItem={cartItem}
+                      setCartItem={setCartItem}
+                    />
+                  ))}
+            </Container>
+          </Container>
+        </>
+      ) : null}
     </>
   );
 }
