63883ebeddccdc531c9f1791c1686af4e6225599
Dushyant Goyal <dushyantgoyal28@gmail.com>
2024-09-20 18:28:29 +0530

    update user routes

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-require('../config/passportConfig')(passport);
+
 const userController = require('../controllers/user.controller');
 
 router.route('/').get(userController.getAll_users);
-router.route('/logout').get(userController.user_logout);
+router.route('/logout').post(userController.user_logout);
 router.route('/profile/:id').get(userController.get_user_profile);
 router.route('/inventory/:id').get(userController.get_user_inventory);
 
